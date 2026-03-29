fcad20533e3e6c1a0fb422f2a24c71253d7d7f1c
clickajans <info@clickajans.net>
2026-03-29 20:39:43 +0300

    Inline remote image loader for Xcode builds

--- Features/Browse/View/NannyCard.swift
+++ Features/Browse/View/NannyCard.swift
@@ -6,6 +6,8 @@
 //
 
 import SwiftUI
+import UIKit
+import Combine
 
 struct NannyCard: View {
     let provider: BrowseProvider
@@ -104,5 +106,62 @@ struct NannyCard: View {
                     .font(.title3.bold())
                     .foregroundStyle(.white)
             }
+    }
+}
+
+struct RemoteImageView<Placeholder: View>: View {
+    let url: URL?
+    let contentMode: ContentMode
+    @ViewBuilder let placeholder: () -> Placeholder
+
+    @StateObject private var loader = RemoteImageLoader()
+
+    init(
+        url: URL?,
+        contentMode: ContentMode = .fill,
+        @ViewBuilder placeholder: @escaping () -> Placeholder
+    ) {
+        self.url = url
+        self.contentMode = contentMode
+        self.placeholder = placeholder
+    }
+
+    var body: some View {
+        Group {
+            if let image = loader.image {
+                Image(uiImage: image)
+                    .resizable()
+                    .aspectRatio(contentMode: contentMode)
+            } else {
+                placeholder()
+            }
+        }
+        .task(id: url) {
+            await loader.load(from: url)
+        }
+    }
+}
+
+@MainActor
+final class RemoteImageLoader: ObservableObject {
+    @Published var image: UIImage?
+
+    func load(from url: URL?) async {
+        guard let url else {
+            image = nil
+            return
+        }
+
+        do {
+            let request = URLRequest(url: url, cachePolicy: .reloadIgnoringLocalCacheData)
+            let (data, response) = try await URLSession.shared.data(for: request)
+            guard let http = response as? HTTPURLResponse, (200...299).contains(http.statusCode) else {
+                image = nil
+                return
+            }
+            image = UIImage(data: data)
+        } catch {
+            image = nil
+        }
     }
 }
